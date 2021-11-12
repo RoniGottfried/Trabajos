5606bf5b643c8d3dd3ad3de17ca797f0c52c38fa
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2021-11-12 10:43:45 -0300

    Index js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-fetch(`https://api.themoviedb.org/3/movie/76341?api_key=2a48645239a84bc2697fe8c66fc6dc00`)
+window.addEventListener('load', function(){
+// series populares 
+fetch(`https://api.themoviedb.org/3/tv/popular?api_key=809187852af3a04706d10c0477580eec`)
     .then(function(response){
         return response.json();
     })
@@ -6,29 +8,69 @@ fetch(`https://api.themoviedb.org/3/movie/76341?api_key=2a48645239a84bc2697fe8c6
 
         console.log(data);
 
-        /* for(let i = 0; i < datos; i++){   
-            document.querySelector('section').innerHTML += `
-                <article>
-                    <h3>${datos.data[i].title}</h3>
-                    <div><img src="${datos.data[i].images.original.url}" alt="Imagen del GIF"></div>
-                    <a href="detalle.html?idGif=${datos.data[i].id}">Ver más información</a>
-                </article>`;
-        }  */
-       /*  document.querySelector('art-genres') */
-        /* for(let i = 0; i < datos.data.length; i++){   
-            document.querySelector('section').innerHTML += `
-                <article>
-                    <h3>${datos.data[i].title}</h3>
-                    <div><img src="${datos.data[i].images.original.url}" alt="Imagen del GIF"></div>
-                    <a href="detalle.html?idGif=${datos.data[i].id}">Ver más información</a>
-                </article>
-            `;   
-        } */
+        for (let i = 0; i < 6; i++){
+            document.querySelector('.series-populares').innerHTML += `
+            <article class="art-peliculas"> 
+                   <a href="detail-serie.html">
+                   <img class= "img-home" src="https://image.tmdb.org/t/p/w342${data.results[i].poster_path}" alt="caratula-vis-a-vis">
+                   </a>
+                   <h2 class="titulos-inicio" >${data.results[i].original_name}</h2>
+               </article>
+            `;
+        }
 
     })
     .catch(function(error){
         console.log(`El error fue: ${error}`);
     })
 
+// peliculas populares
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=809187852af3a04706d10c0477580eec`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
 
+        console.log(data);
+
+        for (let i = 0; i < 6; i++){
+            document.querySelector('.peliculas-populares').innerHTML += `
+            <article class="art-peliculas"> 
+                   <a href="detail-serie.html">
+                   <img class= "img-home" src="https://image.tmdb.org/t/p/w342${data.results[i].poster_path}" alt="caratula-vis-a-vis">
+                   </a>
+                   <h2 class="titulos-inicio" >${data.results[i].title}</h2>
+               </article>
+            `;
+        }
+
+    })
+    .catch(function(error){
+        console.log(`El error fue: ${error}`);
+    })
+
+// lo mas visto 
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=809187852af3a04706d10c0477580eec`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        console.log(data);
+
+        for (let i = 0; i < 6; i++){
+            document.querySelector('.lomasvisto-populares').innerHTML += `
+            <article class="art-peliculas"> 
+                   <a href="detail-serie.html">
+                   <img class= "img-home" src="https://image.tmdb.org/t/p/w342${data.results[i].poster_path}" alt="caratula-vis-a-vis">
+                   </a>
+                   <h2 class="titulos-inicio" >${data.results[i].title}</h2>
+               </article>
+            `;
+        }
+    })
+    .catch(function(error){
+        console.log(`El error fue: ${error}`);
+    })
+})
 
