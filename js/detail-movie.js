90e1939a77082091ccd85348515b89159e12f91d
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2021-11-15 14:15:06 -0300

    detail serie y movie

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,10 +1,10 @@
-
+window.addEventListener('load', function(){
 //  Manipulando la query 
     let queryString = location.search; 
 
     let qsToObject = new URLSearchParams(queryString);
   
-    let movie_id = qsToObject.get('movie_id');
+    let movie_id = qsToObject.get('id');
 
     console.log(movie_id);
    
@@ -18,19 +18,17 @@
             for (let i = 0; i < 1; i++){
                 document.querySelector('.bloque-pelicula').innerHTML += `
                     <section class="imagen-pelicula"> 
-                        <img class="portada-pelicula" src="" alt="portada-shrek">  
+                        <img class="portada-pelicula" src="https://image.tmdb.org/t/p/w342${data.poster_path}" alt="portada-shrek">  
                     </section>
         
                     <section class="peli">
                         <article>
-                            <h1 class="titulo-peli">Shrek Para Siempre</h1>
-                            <p>| Calificacion: 4.8 | Genero: <a href="detail-genres.html">Comedia</a> | Duracion: 1hr 33m | <a href="favourite.html"><i class="far fa-heart"></i></a> </p>
+                            <h1 class="titulo-peli">${data.title}</h1>
+                            <p>| Calificacion: ${data.popularity} | Genero: <a href="detail-genres.html">${data.genres.length}</a> | Duracion: ${data.runtime}| <a href="favourite.html"><i class="far fa-heart"></i></a> </p>
                         </article>
                 
                         <article class="bloque-sinopsis-peli">
-                            <p class="sinopsis">
-                            Instalado en su vida matrimonial y totalmente domesticado, Shrek empieza a extrañar los días en los que él se sentía como un verdadero ogro. Engañado para firmar un contrato con el tortuoso Rumpelstiltskin, él se encuentra a sí mismo en una versión alterna de Muy Muy Lejano, donde los ogros son cazados, Rumpelstiltskin manda, y él y Fiona nunca se conocieron. Shrek debe hallar la forma de cancelar el contrato para restaurar su mundo y reclamar su amor verdadero.
-                            </p>
+                            <p class="sinopsis"> ${data.overview}</p>
                         </article>
                     </section>
                 `;
@@ -66,3 +64,4 @@
         .catch(function(error){
             console.log(`El error fue: ${error}`);
         })
+    })
