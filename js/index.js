726292bb6c7375e8652bd45d663f85fe87307fa3
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2021-11-18 17:28:23 -0300

    validando formularios

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
 window.addEventListener('load', function(){
+// validando formularios 
+let formulario = document.querySelector('form');
+let campo = document.querySelector('.formu');
+let mensajes = document.querySelector('.mensaje');
+
+formulario.addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    if(campo.value == ""){
+        mensajes.innerText = "Campo obligatorio"
+        // chequear si puso mas de tres caracteres
+    }else if (campo.value.length < 3){
+        mensajes.innerText = "Ingresar al menos 3 caracteres"
+
+    }else{
+        formulario.submit()
+    }
+})
+
+// esto se ejecuta siempre, para que este limpio  cuando el usuario ingre al campo --> queremos limpiar el mensaje 
+campo.addEventListener('focus', function(){
+    // limpiar mensaje de error
+    mensajes.innerText = "";
+    // limpiar el valor que tengo en el campo, this habla del elemento donde se verifico el event listener que es el campo
+    this.value = "";
+})
+
+// Obteniendo qs
 
     let queryString = location.search; 
 
@@ -82,5 +110,7 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=809187852af3a04706d1
     .catch(function(error){
         console.log(`El error fue: ${error}`);
     })
+
+
 })
 
