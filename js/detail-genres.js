e75499e1e4aeae9cb97d24c350aacbfb425db6d1
RoniGottfried <rgottfried@udesa.edu.ar>
2021-11-19 11:22:49 -0300

    detaill genres apis y querystring

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,15 +1,19 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString)
-let busqueda = queryStringObj.get("search")
+let busqueda = queryStringObj.get()
 
 let key = "809187852af3a04706d10c0477580eec"
 
-fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=es`)
+document.querySelector(".search").innerHTML += `${busqueda}`;
+
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=es&with_genres=${busqueda}`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
+
+        document.querySelector(".search").innerHTML += `hola`;
 
     })
     .catch(function(error){
@@ -20,17 +24,3 @@ fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=es`
 
 
 
-
-
-
-fetch(`https://api.themoviedb.org/3/genre/${busqueda}?api_key=${key}&language=es`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-
-    })
-    .catch(function(error){
-        console.log(`El error fue: ${error}`);
-    })
