7e49ef13ad62146e6306163901f1b7cda44addde
Felipe Sin <fsin@udesa.edu.ar>
2021-11-22 17:45:43 -0300

    solucionado pagina de favoritos

--- js/favourite.js
+++ js/favourite.js
@@ -2,31 +2,29 @@ window.addEventListener('load', function(){
 
     let favoritos = [];
         
-    // Recuperar los ids del storage.
-    let recuperoStorage = localStorage.getItem('favoritosToString');
-    let favoritosToString = JSON.parse(recuperoStorage);
-
 
     if (localStorage.getItem('favoritosToString')!=null) {
         
-        let favoritos = JSON.parse(localStorage.getItem('favoritosToString'));
+        favoritos = JSON.parse(localStorage.getItem('favoritosToString'));
         console.log(favoritos);
     
         for(let i = 0; i < favoritos.length; i++){
 
             console.log(favoritos[i]);
 
-            fetch(`https://api.themoviedb.org/3/movie/${favoritos[i]}?api_key=809187852af3a04706d10c0477580eec`)
+            fetch(`https://api.themoviedb.org/3/tv/${favoritos[i]}?api_key=809187852af3a04706d10c0477580eec`)
                 .then(function(response){
                     return response.json();
                 })
                 .then(function(datos){
                     console.log(datos);                    
-                    document.querySelector('section').innerHTML += `
-                        <article>
-                            <h3>${datos.data.title}</h3>
-                            <div><img src="${datos.data.images.original.url}" alt="Imagen del GIF"></div>
-                            <a href="detalle.html?idGif=${datos.data.id}">Ver más información</a>
+                    document.querySelector('.series-populares').innerHTML += `
+                        
+                        
+                        <article class="art-peliculas"> 
+                            <a href="detail-serie.html?id=${datos.id}">
+                            <img class= "img-home" src="https://image.tmdb.org/t/p/w342/${datos.poster_path}" alt="caratula"></a>
+                            <h2 class="titulos-inicio">${datos.name}</h2>
                         </article>
                     `;
 
@@ -38,18 +36,69 @@ window.addEventListener('load', function(){
 
         }
     }
-})
+
     
-// else{
+    else{
 
-//     document.querySelector('section').innerHTML += `
-//         <article>
-//             <h3>No has agregado contenido a favoritos</h3>
-//         </article>
-//     `;
+        document.querySelector('section').innerHTML += `
+            <article>
+                <h3>No has agregado contenido a favoritos</h3>
+            </article>
+        `;
 
-// }
+    }
 
+    // Haciendo la sección de favoritos
+
+    let favoritosPeli = [];
+        
+
+    if (localStorage.getItem('favoritosToStringPeli')!=null) {
+        
+        favoritosPeli = JSON.parse(localStorage.getItem('favoritosToStringPeli'));
+        console.log(favoritosPeli);
+    
+        for(let i = 0; i < favoritosPeli.length; i++){
+
+            console.log(favoritosPeli[i]);
+
+            fetch(`https://api.themoviedb.org/3/movie/${favoritosPeli[i]}?api_key=809187852af3a04706d10c0477580eec`)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(datos){
+                    console.log(datos);                    
+                    document.querySelector('.peliculas-populares').innerHTML += `
+                        
+                        
+                        <article class="art-peliculas"> 
+                            <a href="detail-movie.html?id=${datos.id}">
+                            <img class= "img-home" src="https://image.tmdb.org/t/p/w342/${datos.poster_path}" alt="caratula"></a>
+                            <h2 class="titulos-inicio">${datos.title}</h2>
+                        </article>
+                    `;
+
+                })
+                
+                .catch(function(error){
+                    console.log(`El error fue: ${error}`);
+                })
+
+        }
+    }
+
+    
+    else{
+
+        document.querySelector('section').innerHTML += `
+            <article>
+                <h3>No has agregado contenido a favoritos</h3>
+            </article>
+        `;
+
+    }
+
+})
 
 
 
